3969be23398c0ebd0d51a881d94fe1cd8a2316c0
Rodion Smilovskyi <rodion.smilovskyi@mayahtt.com>
2024-02-27 16:58:12 -0500

    Added api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
   const value = 'static web app ts!';
+
+  useEffect(() => {
+    async function loadForecast() {
+      const response = await fetch("/api/WeatherForecast");
+      const data = await response.json();
+      console.log(data);
+    }
+
+    loadForecast();
+
+  }, []);
+
   return <div>Hello {value}</div>;
 }
 
